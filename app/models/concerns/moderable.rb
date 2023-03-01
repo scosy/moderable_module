d860511eefc093dd85d4ac1721cea264aeb58dcb
scosy <amine.bentaleb064@gmail.com>
2023-03-01 11:42:25 +0100

    :card_file_box: Updated Post model and Moderable module
    Included Moderable module in Post model and developped the module

--- app/models/concerns/moderable.rb
+++ app/models/concerns/moderable.rb
@@ -1,5 +1,30 @@
 module Moderable
   extend ActiveSupport::Concern
 
-  
+  included do
+    after_save :moderate
+  end
+
+  class_methods do
+    def moderable_columns(*column_names)
+      @moderated_columns = column_names
+    end
+
+    def moderated_columns
+      @moderated_columns ||= []
+    end
+  end
+
+  private
+
+  def moderate
+    self.class.moderated_columns.each do |column_name|
+      value = self.send(column_name)
+      next if value.blank?
+
+      prediction = JSON.parse(HTTParty.get("http://moderation.logora.fr/predict?text=#{value}").body)["prediction"]["0"]
+      accepted = prediction < 0.5
+      self.update_column(:is_accepted, accepted) unless accepted
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  include Moderable
+
+  moderable_columns(:title, :body)
 end
